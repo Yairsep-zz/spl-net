ceb076fb5063fcc432ad1bf2747cb6fa3791d4df
yairsep <yairsep@gmail.com>
2019-12-31 13:47:15 +0200

    Lab Commit 1

---
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -0,0 +1,40 @@
+package bgu.spl.net.srv;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionsImpl<T> implements Connections<T> {
+
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> connectionsById;
+    private ConcurrentHashMap<String, ConnectionHandler<T>> connectionsByChannel;
+
+    public ConnectionsImpl() {
+        connectionsById = new ConcurrentHashMap<>();
+        connectionsByChannel=new ConcurrentHashMap<>();
+    }
+
+
+    public boolean send(int connectionId, T msg) {
+        ConnectionHandler<T> temp=connectionsById.get(connectionId);
+        if(temp!=null){
+            temp.send(msg);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void send(String channel, T msg) {
+        Iterator <String> itr = connectionsByChannel.keySet().iterator();
+        while(itr.hasNext()) {
+            if(itr.toString()==channel){
+                connectionsByChannel.get(itr).send(msg);
+            }
+        }
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+
+    }
+}
